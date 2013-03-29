e88ca7fbf1f64a961d839fa8505890f44d107362
Adrian Taylor <adrian@macrobug.com>
2013-03-29 15:36:33 +0000

    Tweaks.

--- exercises/Py00-BouncingBall/code/ball.py
+++ exercises/Py00-BouncingBall/code/ball.py
@@ -8,44 +8,45 @@ import pygame
 from pygame.locals import *
 from codeclub_pygame_handy_functions import *
 
-SCREENRECT = Rect(0, 0, 640, 480)
+screen_rect = Rect(0, 0, 640, 480)
 
 pygame.init()
 
-winstyle = 0 # or, could use FULLSCREEN instead of 0
-bestdepth = pygame.display.mode_ok(SCREENRECT.size, 0, 32)
-screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+bestdepth = pygame.display.mode_ok(screen_rect.size, 0, 32)
+screen = pygame.display.set_mode(screen_rect.size, 0, bestdepth)
 
 all = pygame.sprite.RenderUpdates()
 
-BALL_IMAGE = load_image_from_data_directory('ball.png')
+ball_image = load_image_from_data_directory('ball.png')
+small_ball_image = pygame.transform.scale(ball_image, (50, 50))
 
 class Ball(pygame.sprite.Sprite):
     speed = 5
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self, all)
-        self.image = BALL_IMAGE
-        self.rect = self.image.get_rect(midbottom=SCREENRECT.midbottom)
+        self.image = small_ball_image
+        self.rect = self.image.get_rect(midbottom=screen_rect.midbottom)
 
-# Create a ball
-Ball()
+    def move(self, direction):
+        if direction: self.facing = direction
+        self.rect.move_ip(direction*self.speed, 0)
+        self.rect = self.rect.clamp(screen_rect)
 
+background = pygame.Surface(screen_rect.size)
+ball = Ball()
 running = True
 
-while running: # means 'forever'
+while running:
 
-    # See if we want to end the game
     for event in pygame.event.get():
-        if event.type == QUIT or \
-            (event.type == KEYDOWN and event.key == K_ESCAPE):
-                running = False
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            running = False
+    keystate = pygame.key.get_pressed()
+    direction = keystate[K_RIGHT] - keystate[K_LEFT]
+    ball.move(direction)
 
-    # update all the sprites. This calls 'update'
-    # on all the different sprites there are, because we put
-    # them all into the 'all' group above.
-    all.update()
+    all.clear(screen, background)
 
-    #draw the scene
     dirty = all.draw(screen)
     pygame.display.update(dirty)
