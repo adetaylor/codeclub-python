7116f4ef51ef7b11bd4e0122a93ae942c95b4f9c
Adrian Taylor <adrian@macrobug.com>
2013-03-29 12:23:18 +0000

    Moving some stuff into a separate file.

--- exercises/Py01-Alien/code/aliens.py
+++ exercises/Py01-Alien/code/aliens.py
@@ -16,11 +16,11 @@
 # we don't have to do so much work ourselves in our program.
 
 import random # We need use random numbers
-import os.path # We need to use images on disk, and this module helps us specify
-   # how to find them
 import pygame # We are making a game. pygame draws us a window and makes it easy to
    # show sprites.
 from pygame.locals import *  # And we need to get more bits of pygame too.
+from aliens_dataloader import * # Finally some handy bits of code of our own,
+   # which we've just put in a separate file to avoid cluttering this one.
 
 # Check that the version of pygame on this PC is working properly.
 if not pygame.image.get_extended():
@@ -30,8 +30,6 @@ if not pygame.image.get_extended():
 # This code is quite nice and short but there are various aspects of it which I think we should
 # try to simplify for CodeClub.
 # Specifically:
-# 1. We need big obvious section headings about which bits the kids should ignore versus not.
-#    e.g. I have no intention to teach them about modules, etc just yet.
 # 2. It would be better if all the code for each sprite is localised. That probably means adding
 #    a load_images method which does just that for each sprite, instead of specifying the
 #    image filenames within main.
@@ -40,7 +38,6 @@ if not pygame.image.get_extended():
 # 4. Consider hiding some boilerplate in a module (e.g. the image loading stuff). Try to keep
 #    this file fully oriented towards alien-related topics.
 # 5. Get rid of 'animcycle'
-# 6. Simply more comments, ideally on nearly every line.
 
 #######################################################################################
 # Basic facts about the game.
@@ -54,51 +51,6 @@ ALIEN_RELOAD   = 12     # frames between new aliens (25 frames per second, so th
                         # be an alien every 0.5 seconds (ish) depending on the odds)
 SCREENRECT     = Rect(0, 0, 640, 480)
 SCORE          = 0
-
-#######################################################################################
-# Stuff about loading images and sounds.
-# Scratch intrinsically knows how to show images and play sounds, but Python
-# doesn't so we have to have this code to load them. However, fortunately,
-# we use the services of another program called 'pygame' to do most of the work.
-#######################################################################################
-
-# Set up a variable containing the location of this 'aliens.py' file on disk.
-# We assume all the images are nearby in a folder called 'data'.
-main_dir = os.path.split(os.path.abspath(__file__))[0]
-
-def load_image(file):
-    "loads an image, prepares it for play"
-    file = os.path.join(main_dir, 'data', file)
-    try:
-        surface = pygame.image.load(file)
-    except pygame.error:
-        raise SystemExit('Could not load image "%s" %s'%(file, pygame.get_error()))
-    return surface.convert_alpha()
-
-def load_images(*files):
-    imgs = []
-    for file in files:
-        imgs.append(load_image(file))
-    return imgs
-
-# Aha, you *are* reading this in detail.
-# A 'class' in Python is a bit like a sprite in Scratch, except that it doesn't
-# actually have to be something you can see on the screen. Any 'thing' can
-# be a class. For example a sound can be a class. This particular 'thing'
-# is an empty sound that can be played instead of a proper sound if
-# the proper sound can't be loaded.
-class dummysound:
-    def play(self): pass
-
-def load_sound(file):
-    if not pygame.mixer: return dummysound()
-    file = os.path.join(main_dir, 'data', file)
-    try:
-        sound = pygame.mixer.Sound(file)
-        return sound
-    except pygame.error:
-        print ('Warning, unable to load, %s' % file)
-    return dummysound()
 
 #######################################################################################
 # Game objects.
