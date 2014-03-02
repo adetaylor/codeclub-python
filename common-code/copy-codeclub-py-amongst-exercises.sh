1e7ff57a29c70662856e4f19173d907fe60aa4ad
Adrian Taylor <adrian@bromium.com>
2014-03-02 21:16:58 +0000

    Adding script to update common code.

---
+++ common-code/copy-codeclub-py-amongst-exercises.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp codeclub.py ../exercises/Py01-Ball/code-problem/codeclub.py
+cp codeclub.py ../exercises/Py02-Alien/code-problem/codeclub.py
+cp codeclub.py ../exercises/Py04-Felix/code-problem/codeclub.py
+cp codeclub.py ../exercises/Py07-Airport/code-problem/codeclub.py
